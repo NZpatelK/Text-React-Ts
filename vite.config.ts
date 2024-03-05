eab5f54c9b5931c05207b18bfe85c62ad2d0bf21
NZpatelK <patelk.dev@hotmail.com>
2024-03-05 17:34:58 +1300

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react' 
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/Text-React-Ts/",
-  plugins: [],
+  plugins: [react()],
   resolve: {
     alias: [
       
