40ad5f41a3b31764b9212f12194330fa2512aac4
NZpatelK <patelk.dev@hotmail.com>
2024-03-08 11:52:56 +1300

    fix the home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,41 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Home.css';
+
 
 const Home: React.FC = () => {
     return (
-        <div>
-            <h1>Welcome to the Home Page</h1>
-            <p>This is the content of the Text Style and Animation</p>
-            <Link to="/Text-React-Ts/ZoomBySwitch">Go to Zoom by switch using button click</Link>   
-            <br />
-            <Link to="/Text-React-Ts/ZoomBySwitch2">Go to Zoom by switch by using scroll</Link>
-            <br />
-            <Link to="LetterRotating">Go to Letter Rotating</Link>
-            <br />
-            <Link to="MultipleWordsZoom">Go to Mutliple words zoom</Link>
-            <br />
-            <Link to="SlidesZoom">Go to Mutliple words zoom</Link>
+        <div className='homepage'>
+            <h1 style={{ margin: "0", textAlign: "center" }}>Welcome to the Text Components</h1>
+            <p style={{ margin: "2px 0", textAlign: "center" }}>There are various components of text and it has amination and style. </p>
+
+            <div className='LinkContainer'>
+                <div className='LinkItem'>
+                    <h2>Zoom by Switch</h2>
+                    <p>Zoom by switch using button click to click next or back</p>
+                    <Link to="ZoomBySwitch" className='Link'>Click Here</Link>
+                </div>
+                <div className='LinkItem'>
+                    <h2>Zoom by Switch</h2>
+                    <p>Zoom by switch using button click to click next or back</p>
+                    <Link className='Link' to="ZoomBySwitch2">Go to Zoom by switch by using scroll</Link>
+                </div>
+                <div className='LinkItem'>
+                    <h2>Zoom by Switch</h2>
+                    <p>Zoom by switch using button click to click next or back</p>
+                    <Link className='Link' to="LetterRotating">Go to Letter Rotating</Link>
+                </div>
+                <div className='LinkItem'>
+                    <h2>Zoom by Switch</h2>
+                    <p>Zoom by switch using button click to click next or back</p>
+                    <Link className='Link' to="MultipleWordsZoom">Go to Mutliple words zoom</Link>
+                </div>
+                <div className='LinkItem'>
+                    <h2>Zoom by Switch</h2>
+                    <p>Zoom by switch using button click to click next or back</p>
+                    <Link className='Link' to="SlidesZoom">Go to Slide zoom</Link>
+                </div>
+            </div>
         </div>
     );
 };
