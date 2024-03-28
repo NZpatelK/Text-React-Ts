a8ef76e05e8b45628aa06d2a7a24218c03fd9d4c
NZpatelK <patelk.dev@hotmail.com>
2024-03-28 21:00:25 +1300

    add another odometer file

--- src/components/OdometerNumber/OdometerNumber.tsx
+++ src/components/OdometerNumber/OdometerNumber.tsx
@@ -1,9 +1,15 @@
+import { useState, useEffect } from "react";
+import Odometer from 'react-odometerjs';
 
 export const OdometerNumber = () => {
+    const [value, setValue] = useState(1234);
 
+    useEffect(() => {
+        const timeoutId = setTimeout(() => setValue(4321), 2000);
+        return () => {
+            clearTimeout(timeoutId);
+        };
+    }, []);
 
-
-    return <div className="odometer">
-
-    </div>
+    return <Odometer value={value} format="(.ddd),dd" />;
 }
