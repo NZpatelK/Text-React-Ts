c598c0c4844732784bdaba1e7a0594bfb0ffb82e
NZpatelK <patelk.dev@hotmail.com>
2024-03-29 14:55:03 +1300

    Update OdometerNumber.tsx

--- src/components/OdometerNumber/OdometerNumber.tsx
+++ src/components/OdometerNumber/OdometerNumber.tsx
@@ -3,6 +3,14 @@ import Odometer from 'react-odometerjs';
 
 export const OdometerNumber = () => {
     const [value, setValue] = useState(111);
+    const [theme, setTheme] = useState("digital");
+
+    const listTheme = ["default",  "car", "plaza", "slot-machine", "digital", "train-station"];
+
+    const changeTheme = (theme: string) => {
+        setTheme(theme);
+        console.log(theme);
+    }
 
     useEffect(() => {
         const timeoutId = setTimeout(() => setValue(4321), 2000);
@@ -11,5 +19,15 @@ export const OdometerNumber = () => {
         };
     }, []);
 
-    return <Odometer value={value} format="d" style={{fontSize: "10em"}} theme="car" />;
+    return (
+        <div>
+            <Odometer value={value} format="d" style={{ fontSize: "10em" }} theme={theme} />
+            <div className="theme-option">
+                {listTheme.map((theme, index) => (
+                    <button key={index} onClick={() => changeTheme(theme)}>{theme}</button>
+                ))}
+            </div>
+            <input type="number" onChange={(e) => setValue(parseInt(e.target.value))} />
+        </div>
+    );
 }
