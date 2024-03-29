40fa2e81038f7bf9c9571f1eed15c57d4f1a984b
NZpatelK <patelk.dev@hotmail.com>
2024-03-29 21:26:03 +1300

    Update OdometerNumber.tsx

--- src/components/OdometerNumber/OdometerNumber.tsx
+++ src/components/OdometerNumber/OdometerNumber.tsx
@@ -3,14 +3,17 @@ import Odometer from 'react-odometerjs';
 
 export const OdometerNumber = () => {
     const [value, setValue] = useState(111);
-    const [theme, setTheme] = useState("digital");
+    const [theme, setTheme] = useState(0);
+    const [isDisplay, setIsDisplay] = useState(false);
 
     const listTheme = ["default",  "car", "plaza", "slot-machine", "digital", "train-station"];
 
-    const changeTheme = (theme: string) => {
-        setTheme(theme);
-        console.log(theme);
+    const changeTheme = (index: number) => {
+        setTheme(index);
+        setIsDisplay(true);
     }
+
+    console.log("theme", theme);
 
     useEffect(() => {
         const timeoutId = setTimeout(() => setValue(4321), 2000);
@@ -18,16 +21,18 @@ export const OdometerNumber = () => {
             clearTimeout(timeoutId);
         };
     }, []);
+    
+    // return(<> <Odometer value={value} format="d" style={{ fontSize: "10em" }} theme={theme}/></>)
 
     return (
-        <div>
-            <Odometer value={value} format="d" style={{ fontSize: "10em" }} theme={theme} />
+        <>
+            <Odometer value={value} format="d" style={isDisplay ? {fontSize: "10em", cursor: 'pointer' } : {display: "none"}} theme={listTheme[theme]}  />
             <div className="theme-option">
                 {listTheme.map((theme, index) => (
-                    <button key={index} onClick={() => changeTheme(theme)}>{theme}</button>
+                    <button key={index} onClick={() => changeTheme(index)}>{theme}</button>
                 ))}
             </div>
             <input type="number" onChange={(e) => setValue(parseInt(e.target.value))} />
-        </div>
+        </>
     );
 }
