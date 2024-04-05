2f00563a8d50b1de544d0daf4e443068d621d50d
NZpatelK <patelk.dev@hotmail.com>
2024-04-06 11:47:52 +1300

    create B letter svg

--- src/components/TextSvgAnimation/TextSvgAnimation.tsx
+++ src/components/TextSvgAnimation/TextSvgAnimation.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import A from '../../assets/alphabet/A.svg';
+import B from '../../assets/alphabet/B.svg';
+import letters from '../../assets/alphabet/letters.svg';
 
 export const TextSvgAnimation = () => {
   return (
-    <img src={A} alt="My SVG" />
+   <>
+        {/* <img src={A} alt="My SVG" width={100} height={100} style={{background: "#000"}} /> */}
+        {/* <img src={B} alt="My SVG" width={100} height={100} style={{background: "#000"}} /> */}
+        <img src={letters} alt="My SVG" width={500} height={500} />
+   </>
   )
 }
