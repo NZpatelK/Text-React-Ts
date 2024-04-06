2ceb9ec8ad545177961e327b845a5b62e1b4f3ff
NZpatelK <patelk.dev@hotmail.com>
2024-04-06 13:02:39 +1300

    Implement C letter svg

--- src/components/TextSvgAnimation/TextSvgAnimation.tsx
+++ src/components/TextSvgAnimation/TextSvgAnimation.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import A from '../../assets/alphabet/A.svg';
 import B from '../../assets/alphabet/B.svg';
+import C from '../../assets/alphabet/C.svg';
 import letters from '../../assets/alphabet/letters.svg';
 
 export const TextSvgAnimation = () => {
@@ -8,7 +9,8 @@ export const TextSvgAnimation = () => {
    <>
         {/* <img src={A} alt="My SVG" width={100} height={100} style={{background: "#000"}} /> */}
         {/* <img src={B} alt="My SVG" width={100} height={100} style={{background: "#000"}} /> */}
-        <img src={letters} alt="My SVG" width={500} height={500} />
+        <img src={C} alt="My SVG" width={100} height={100} style={{background: "#000"}} />
+        {/* <img src={letters} alt="My SVG" width={500} height={500} /> */}
    </>
   )
 }
