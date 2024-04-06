863c21877d626e9973cf63efea7edcc84f75ad56
NZpatelK <patelk.dev@hotmail.com>
2024-04-06 13:10:39 +1300

    implement F letter svg

--- src/components/TextSvgAnimation/TextSvgAnimation.tsx
+++ src/components/TextSvgAnimation/TextSvgAnimation.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import A from '../../assets/alphabet/A.svg';
-import B from '../../assets/alphabet/B.svg';
-import C from '../../assets/alphabet/C.svg';
-import D from '../../assets/alphabet/D.svg';
-import E from '../../assets/alphabet/E.svg';
-// import F from '../../assets/alphabet/F.svg';
-import letters from '../../assets/alphabet/letters.svg';
+// import A from '../../assets/alphabet/A.svg';
+// import B from '../../assets/alphabet/B.svg';
+// import C from '../../assets/alphabet/C.svg';
+// import D from '../../assets/alphabet/D.svg';
+// import E from '../../assets/alphabet/E.svg';
+import F from '../../assets/alphabet/F.svg';
+// import letters from '../../assets/alphabet/letters.svg';
 
 export const TextSvgAnimation = () => {
   return (
@@ -14,7 +14,8 @@ export const TextSvgAnimation = () => {
         {/* <img src={B} alt="My SVG" width={100} height={100} style={{background: "#000"}} /> */}
         {/* <img src={C} alt="My SVG" width={100} height={100} style={{background: "#000"}} /> */}
         {/* <img src={D} alt="My SVG" width={100} height={100} style={{background: "#000"}} /> */}
-        <img src={E} alt="My SVG" width={100} height={100} style={{background: "#000"}} />
+        {/* <img src={E} alt="My SVG" width={100} height={100} style={{background: "#000"}} /> */}
+        <img src={F} alt="My SVG" width={100} height={100} style={{background: "#000"}} />
         {/* <img src={letters} alt="My SVG" width={500} height={500} /> */}
    </>
   )
