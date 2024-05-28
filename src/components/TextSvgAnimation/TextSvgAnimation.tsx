cfb47e6f9590deef499f11e8dda0cec3c27717f0
NZpatelK <patelk.dev@hotmail.com>
2024-05-28 12:10:46 +1200

    Update: modify A

--- src/components/TextSvgAnimation/TextSvgAnimation.tsx
+++ src/components/TextSvgAnimation/TextSvgAnimation.tsx
@@ -1,33 +1,4 @@
 import React from 'react'
-// import A from '../../assets/alphabet/A.svg';
-// import B from '../../assets/alphabet/B.svg';
-// import C from '../../assets/alphabet/C.svg';
-// import D from '../../assets/alphabet/D.svg';
-// import E from '../../assets/alphabet/E.svg';
-// import F from '../../assets/alphabet/F.svg';
-// import G from '../../assets/alphabet/G.svg';
-// import H from '../../assets/alphabet/H.svg';
-// import I from '../../assets/alphabet/I.svg';
-// import J from '../../assets/alphabet/J.svg';
-// import K from '../../assets/alphabet/K.svg';
-// import L from '../../assets/alphabet/L.svg';
-// import M from '../../assets/alphabet/M.svg';
-// import N from '../../assets/alphabet/N.svg';
-// import O from '../../assets/alphabet/O.svg';
-// import P from '../../assets/alphabet/P.svg';
-// import Q from '../../assets/alphabet/Q.svg';
-// import R from '../../assets/alphabet/R.svg';
-// import S from '../../assets/alphabet/S.svg';
-// import T from '../../assets/alphabet/T.svg';
-// import U from '../../assets/alphabet/U.svg';
-// import V from '../../assets/alphabet/V.svg';
-// import W from '../../assets/alphabet/W.svg';
-// import X from '../../assets/alphabet/X.svg';
-// import Y from '../../assets/alphabet/Y.svg';
-// import Z from '../../assets/alphabet/Z.svg';
-
-// import letters from '../../assets/alphabet/1letters.svg';
-
 import './TextSvgAnimation.css';
 import { Z } from './alphabet/Z';
 import { A } from './alphabet/A';
@@ -85,38 +56,6 @@ export const TextSvgAnimation: React.FC = () => {
       <X />
       <Y />
       <Z />
-
-
-
-
-      {/* <img src={A} alt="My SVG" width={100} height={100} style={{background: "#000"}} /> */}
-      {/* <img src={B} alt="My SVG" width={100} height={100} style={{background: "#000"}} /> */}
-      {/* <img src={C} alt="My SVG" width={100} height={100} style={{background: "#000"}} /> */}
-      {/* <img src={D} alt="My SVG" width={100} height={100} style={{background: "#000"}} /> */}
-      {/* <img src={E} alt="My SVG" width={100} height={100} style={{background: "#000"}} /> */}
-      {/* <img src={F} alt="My SVG" width={100} height={100} style={{background: "#000"}} /> */}
-      {/* <img src={G} alt="My SVG" width={100} height={100} style={{background: "#000"}} /> */}
-      {/* <img src={H} alt="My SVG" width={100} height={100} style={{background: "#000"}} /> */}
-      {/* <img src={I} alt="My SVG" width={100} height={100} style={{background: "#000"}} /> */}
-      {/* <img src={J} alt="My SVG" width={100} height={100} style={{background: "#000"}} /> */}
-      {/* <img src={K} alt="My SVG" width={100} height={100} style={{background: "#000"}} /> */}
-      {/* <img src={L} alt="My SVG" width={100} height={100} style={{background: "#000"}} /> */}
-      {/* <img src={M} alt="My SVG" width={100} height={100} style={{background: "#000"}} /> */}
-      {/* <img src={N} alt="My SVG" width={100} height={100} style={{background: "#000"}} /> */}
-      {/* <img src={O} alt="My SVG" width={100} height={100} style={{background: "#000"}} /> */}
-      {/* <img src={P} alt="My SVG" width={100} height={100} style={{background: "#000"}} /> */}
-      {/* <img src={Q} alt="My SVG" width={100} height={100} style={{background: "#000"}} /> */}
-      {/* <img src={R} alt="My SVG" width={100} height={100} style={{background: "#000"}} /> */}
-      {/* <img src={S} alt="My SVG" width={100} height={100} style={{background: "#000"}} /> */}
-      {/* <img src={T} alt="My SVG" width={100} height={100} style={{background: "#000"}} /> */}
-      {/* <img src={U} alt="My SVG" width={100} height={100} style={{background: "#000"}} /> */}
-      {/* <img src={V} alt="My SVG" width={100} height={100} style={{background: "#000"}} /> */}
-      {/* <img src={W} alt="My SVG" width={100} height={100} style={{background: "#000"}} /> */}
-      {/* <img src={X} alt="My SVG" width={100} height={100} style={{background: "#000"}} /> */}
-      {/* <img src={Y} alt="My SVG" width={100} height={100} style={{background: "#000"}} /> */}
-      {/* <div className='loading-page'><img src={Z} className='svg' alt="My SVG" width={"150px"} height={"150px"}/></div> */}
-
-      {/* <img src={letters} alt="My SVG" width={500} height={500} /> */}
     </>
   )
 }
